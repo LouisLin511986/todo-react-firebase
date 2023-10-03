d0a01caa55049bf1ba2fea6620be7cf965725dd9
Louis511986 <90818126@gcloud.csu.edu.tw>
2023-10-03 09:59:13 +0800

    0959

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4hYJ5V3tD0XMFD-QtOijDO_3iOwQKjEQ",
-  authDomain: "todo-react-firebase-1a17b.firebaseapp.com",
-  projectId: "todo-react-firebase-1a17b",
-  storageBucket: "todo-react-firebase-1a17b.appspot.com",
-  messagingSenderId: "1053703561549",
-  appId: "1:1053703561549:web:e749c859726acd4332d704"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
